8e80c919f8bd403eee5d8fb37872720b04173ef5
Volkowo <j_knz12@hotmail.com>
2023-10-30 14:59:28 +1000

    We can collect resource and spawn minions now!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let ship, resourceNodes, base;
+let resourceCounter = 0;
+let spawnBoolean = false;
 
 function preload(){
 
@@ -12,12 +14,12 @@ function setup(){
     base.h = 60;
 
     resourceNodes = new Group();
-    resourceNodes.d = 50;
-    resourceNodes.y = 50;
-    while (resourceNodes.length < 3) {
-        let resourceNode = new resourceNodes.Sprite();
-        resourceNode.x = resourceNodes.length * 150;
-    }
+    // resourceNodes.d = 50;
+    // resourceNodes.y = 50;
+    // while (resourceNodes.length < 3) {
+    //     let resourceNode = new resourceNodes.Sprite();
+    //     resourceNode.x = resourceNodes.length * 150;
+    // }
 
     ship = new Group();
     ship.w = 30;
@@ -25,9 +27,54 @@ function setup(){
 }
 
 function draw(){
-    background(125)
+    background(125);
 
-    if (kb.presses('o')){
+    textSize(22);
+    text("Resource Collected: " + resourceCounter, 50, 700);
+
+    spawnResources();
+    collectResources();
+    spawnMinions();
+}
+
+function spawnResources(){
+    if(spawnBoolean == false){
+        for(let i = 0; resourceNodes.length < 8; i++){
+            resourceNodes.add(createResource());
+            console.log(resourceNodes.length);
+        }
+        spawnBoolean = true;
+    } 
+}
+
+function createResource(){
+    let resourceNode = new Sprite();
+    resourceNode.d = 50;
+    resourceNode.y = 50;
+    resourceNode.x = (resourceNodes.length * 100) + 50;
+    resourceNode.collider = 'static'
+
+    return resourceNode;
+}
+
+function collectResources(){
+    for(let i = 0; i < resourceNodes.length; i++){
+        // console.log(dist(mouseX, mouseY, resourceNodes[0].x, resourceNodes[0].y))
+        if(mouse.pressing() && dist(mouseX, mouseY, resourceNodes[i].x, resourceNodes[i].y) < resourceNodes[0].d){
+            resourceNodes[i].remove();
+            resourceCounter += 1;
+            console.log(resourceCounter);
+        }
+    }
+}
+
+let warningText = false;
+
+function spawnMinions(){
+    if (kb.presses('o') && resourceCounter >= 3){
         new ship.Sprite(width/2, height/2-50);
+        resourceCounter -= 3;
+    } else {
+        text("You need to collect at least 3 resources to spawn a ship!", 50 , 750)
     }
 }
