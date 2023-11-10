c23cf714d09e06463d5186ae270498620401ba17
rontht <thwinhtoothan@gmail.com>
2023-11-10 11:58:39 +1000

    mid Menu Screen

--- unrefined_features/main.js
+++ unrefined_features/main.js
@@ -10,13 +10,13 @@ const GAME = 3;
 const CREDITS = 4;
 
 // what screen is loaded upon launch/refresh
-let currentScreen = GAME;
+let currentScreen = LOADING;
 
 // button variables
-let playButton;
-let quitButton;
-let creditsButton;
-let backToMenuButton;
+// let playButton;
+// let quitButton;
+// let creditsButton;
+// let backToMenuButton;
 
 // classes
 let screenLoading = new Loading();
@@ -29,8 +29,16 @@ let spriteLogic = new Logic();
 
 // other variables
 let isGameActive = false;
+let movingAni = 500;
+let startMenuAni = false;
+let openSetting = false;
+let activateSetting = false;
 
 function preload() {
+    screenMenu.preload();
+    screenLoading.preload();
+    screenPressAnyKey.preload();
+    screenCredits.preload();
     createSprite.preload();
     gameUI.preload();
     spriteLogic.preload();
@@ -38,7 +46,10 @@ function preload() {
 
 function setup() {
     new Canvas(W, H);
-    buttonCreation();
+    screenMenu.setup();
+    screenLoading.setup();
+    screenPressAnyKey.setup();
+    screenCredits.setup();
 }
 
 function draw() {
@@ -46,71 +57,50 @@ function draw() {
 
     // text(spriteLogic.currentX, 500, 100)
     // text(spriteLogic.currentY, 600, 100)
-    
+
     // switches screen states
     switch (currentScreen) {
         case LOADING:
-            drawLoadingScreen();
+            screenLoading.draw();
             break;
         case PRESS_ANY_KEY:
-            drawPressAnyKeyScreen();
+            screenPressAnyKey.draw();
             break;
         case MENU:
-            drawMenuScreen();
+            screenMenu.draw();
             break;
         case GAME:
             drawGameScreen();
             break;
         case CREDITS:
-            drawCreditsScreen();
+            screenCredits.draw();
             break;
     }
-
-    // if (frameCount === 150) { // loading screen stops after 150 frames
-    //     currentScreen = PRESS_ANY_KEY;
-    // }
 }
 
 function keyPressed() { // change from press any key to menu
     if (currentScreen === PRESS_ANY_KEY) {
         currentScreen = MENU;
-        playButton.show();
-        creditsButton.show();
+        screenMenu.enableMenuButtons();
     }
-}
-
-function drawLoadingScreen() { // loading screen code
-    screenLoading.draw();
-}
-
-function drawPressAnyKeyScreen() { // press any key to continue code
-    screenPressAnyKey.draw();
-}
-
-function drawMenuScreen() { // menu screen code
-    screenMenu.draw();
-}
-
-function establishGame() { // class set up code here
-    createSprite.setup();
-    gameUI.setup(createSprite);
-    spriteLogic.setup(createSprite);
 }
 
 function drawGameScreen() { // game screen code
     if (isGameActive === false) {
-        establishGame();
+        createSprite.setup();
+        gameUI.setup(createSprite);
+        spriteLogic.setup(createSprite);
         isGameActive = true;
     }
-    
+
     // class draw code here
     createSprite.draw();
 
     //connecting the gameUI to createSprite
     gameUI.factory = createSprite;
     gameUI.logic = spriteLogic;
-    
-    
+
+
     //to make sure the logic is using the same groups and sprites from gameUI
     spriteLogic.resource = gameUI.resource;
     // spriteLogic.selection = gameUI.selection;
@@ -121,65 +111,4 @@ function drawGameScreen() { // game screen code
     spriteLogic.draw(createSprite);
 
     gameUI.draw(createSprite);
-}
-
-function drawCreditsScreen() { // leaderboard screen code
-    screenCredits.draw();
-}
-
-function buttonCreation() {
-    playButton = createButton('Start Game');
-    playButton.mouseClicked(playButtonClicked);
-    playButton.size(100, 50);
-    playButton.position(width / 2 - playButton.size().width / 2, height / 2);
-    playButton.hide();
-
-    quitButton = createButton('Quit Game');
-    quitButton.mouseClicked(quitButtonClicked);
-    quitButton.size(100, 50);
-    quitButton.position(100, 100);
-    quitButton.hide();
-
-    creditsButton = createButton('Credits');
-    creditsButton.mouseClicked(creditsButtonClicked);
-    creditsButton.size(100, 50);
-    creditsButton.position(width / 2 - creditsButton.size().width / 2, (height / 2) + 100);
-    creditsButton.hide();
-
-    backToMenuButton = createButton('Back to Main Menu');
-    backToMenuButton.mouseClicked(backToMenuButtonClicked);
-    backToMenuButton.size(100, 50);
-    backToMenuButton.position(100, 100);
-    backToMenuButton.hide();
-}
-
-function playButtonClicked() {
-    currentScreen = GAME;
-    playButton.hide();
-    quitButton.show();
-    creditsButton.hide();
-}
-
-function quitButtonClicked() { // reset game parameters here (needs to be added)
-    currentScreen = MENU;
-    playButton.show();
-    quitButton.hide();
-    creditsButton.show();
-
-    isGameActive = false;
-    allSprites.remove();
-}
-
-function creditsButtonClicked() {
-    currentScreen = CREDITS;
-    playButton.hide();
-    creditsButton.hide();
-    backToMenuButton.show();
-}
-
-function backToMenuButtonClicked() {
-    currentScreen = MENU;
-    playButton.show();
-    creditsButton.show();
-    backToMenuButton.hide();
 }
--- unrefined_features/menu.js
+++ unrefined_features/menu.js
@@ -4,15 +4,30 @@ class Loading {
     }
 
     preload() {
-
+        this.loadingImg = loadImage("assets/menu/loading.jpg");
     }
 
     setup() {
-
     }
 
     draw() {
-        text('loading', 75, 75);
+        //Background Image
+        image(this.loadingImg, 0, 0, W, H);
+
+        //Loading Text
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        fill('white');
+        text('loading', W / 2, 75);
+
+        //Revert Back to Normal
+        textAlign(CENTER, CENTER);
+        textSize(10);
+        fill('black');
+
+        if (frameCount === 150 && currentScreen == LOADING) { // loading screen stops after 150 frames
+            currentScreen = PRESS_ANY_KEY;
+        }
     }
 }
 
@@ -22,7 +37,7 @@ class PressAnyKey {
     }
 
     preload() {
-
+        this.loadingImg = loadImage("assets/menu/loading.jpg");
     }
 
     setup() {
@@ -30,7 +45,24 @@ class PressAnyKey {
     }
 
     draw() {
-        text('press any key to continue', 75, 75);
+        //Background Image
+        image(this.loadingImg, 0, 0, W, H);
+
+        //Loading Text
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        fill('white');
+        text('press any key to continue', W / 2, 75);
+
+        //Revert Back to Normal
+        textAlign(CENTER, CENTER);
+        textSize(10);
+        fill('black');
+
+        if (mouse.presses('left') || mouse.presses('right')) {
+            currentScreen = MENU;
+            screenMenu.enableMenuButtons();
+        }
     }
 }
 
@@ -40,15 +72,313 @@ class Menu {
     }
 
     preload() {
-
+        this.menuImg = loadImage("assets/menu/menu.jpg");
+        this.settingButtonImg = loadImage("assets/menu/settingButton.png");
+        this.creditsButtonImg = loadImage("assets/menu/creditsButton.png");
+        this.playButtonImg = loadImage("assets/menu/playButton.png");
+        this.shadeImg = loadImage("assets/menu/buttonShade.png");
     }
 
     setup() {
-
+        // this.buttonCreation();
+        this.buttonSprites();
+        this.settingSprites();
     }
 
     draw() {
-        text('menu screen', 75, 75);
+        //Background Image
+        image(this.menuImg, 0, 0, W, H);
+
+        //Game Title
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        fill('white');
+        text('menu screen', W / 2, 75);
+
+        //Revert Back to Normal
+        textAlign(CENTER, CENTER);
+        textSize(10);
+        fill('black');
+
+        this.buttonSpritesFunctions();
+    }
+
+    // buttonCreation() {
+    //     this.playButton = createButton('Start Game');
+    //     this.playButton.mouseClicked(() => {this.playButtonClicked()});
+    //     this.playButton.size(100, 50);
+    //     this.playButton.position(W / 2 - this.playButton.size().width / 2, H / 2);
+    //     this.playButton.show();
+
+    //     // this.quitButton = createButton('Quit Game');
+    //     // this.quitButton.mouseClicked(() => {this.quitButtonClicked()});
+    //     // this.quitButton.size(100, 50);
+    //     // this.quitButton.position(100, 100);
+    //     // this.quitButton.hide();
+
+    //     this.settingButton = createButton('Settings');
+    //     this.settingButton.mouseClicked(() => {this.settingButtonClicked()});
+    //     this.settingButton.size(100, 50);
+    //     this.settingButton.position(W / 2 - this.settingButton.size().width / 2, (H / 2) + 100);
+    //     this.settingButton.show();
+
+    //     this.creditsButton = createButton('Credits');
+    //     this.creditsButton.mouseClicked(() => {this.creditsButtonClicked()});
+    //     this.creditsButton.size(100, 50);
+    //     this.creditsButton.position(W / 2 - this.creditsButton.size().width / 2, (H / 2) + 200);
+    //     this.creditsButton.show();
+
+    //     this.backToMenuButton = createButton('Back to Main Menu');
+    //     this.backToMenuButton.mouseClicked(() => {this.backToMenuButtonClicked()});
+    //     this.backToMenuButton.size(100, 50);
+    //     this.backToMenuButton.position(W / 2 - this.backToMenuButton.size().width / 2, (H / 2) + 200);
+    //     this.backToMenuButton.hide();
+    // }
+
+    buttonSprites() {
+        this.playButtonSprite = new Sprite(W / 2 - 200, H - 150);
+        this.playButtonSprite.w = 166; //128
+        this.playButtonSprite.h = 240; //185
+        this.playButtonSprite.color = 'red';
+        this.playButtonSprite.draw = () => {
+            let hoverEffect = 0;
+            if (mouseX > this.playButtonSprite.x - this.playButtonSprite.w / 2
+                && mouseX < this.playButtonSprite.x + this.playButtonSprite.w / 2
+                && mouseY > this.playButtonSprite.y - this.playButtonSprite.h / 2
+                && mouseY < this.playButtonSprite.y + this.playButtonSprite.h / 2) {
+                hoverEffect = -5;
+            } else {
+                hoverEffect = 0;
+            }
+            image(this.shadeImg, 0, 0 + movingAni, 166, 240);
+            image(this.playButtonImg, hoverEffect, hoverEffect + movingAni, 166, 240);
+        }
+        this.playButtonSprite.visible = true;
+        this.playButtonSprite.collider = 'k';
+
+        this.settingButtonSprite = new Sprite(W / 2, H - 150);
+        this.settingButtonSprite.w = 166;
+        this.settingButtonSprite.h = 240;
+        this.settingButtonSprite.color = 'red';
+        this.settingButtonSprite.draw = () => {
+            let hoverEffect = 0;
+            if (mouseX > this.settingButtonSprite.x - this.settingButtonSprite.w / 2
+                && mouseX < this.settingButtonSprite.x + this.settingButtonSprite.w / 2
+                && mouseY > this.settingButtonSprite.y - this.settingButtonSprite.h / 2
+                && mouseY < this.settingButtonSprite.y + this.settingButtonSprite.h / 2) {
+                hoverEffect = -5;
+            } else {
+                hoverEffect = 0;
+            }
+            image(this.shadeImg, 0, 0 + movingAni, 166, 240);
+            image(this.settingButtonImg, hoverEffect, hoverEffect + movingAni, 166, 240);
+        }
+        this.settingButtonSprite.visible = true;
+        this.settingButtonSprite.collider = 'k';
+
+        this.creditsButtonSprite = new Sprite(W / 2 + 200, H - 150);
+        this.creditsButtonSprite.w = 166;
+        this.creditsButtonSprite.h = 240;
+        this.creditsButtonSprite.color = 'red';
+        this.creditsButtonSprite.draw = () => {
+            let hoverEffect = 0;
+            if (mouseX > this.creditsButtonSprite.x - this.creditsButtonSprite.w / 2
+                && mouseX < this.creditsButtonSprite.x + this.creditsButtonSprite.w / 2
+                && mouseY > this.creditsButtonSprite.y - this.creditsButtonSprite.h / 2
+                && mouseY < this.creditsButtonSprite.y + this.creditsButtonSprite.h / 2) {
+                hoverEffect = -5;
+            } else {
+                hoverEffect = 0;
+            }
+            image(this.shadeImg, 0, 0 + movingAni, 166, 240);
+            image(this.creditsButtonImg, hoverEffect, hoverEffect + movingAni, 166, 240);
+        }
+        this.creditsButtonSprite.visible = true;
+        this.creditsButtonSprite.collider = 'k';
+
+        this.closeSettingButton = createButton('x');
+        this.closeSettingButton.position(950, 150);
+        this.closeSettingButton.mouseClicked(() => { this.closeSettingClicked() });
+        this.closeSettingButton.style("width", "50px");
+        this.closeSettingButton.style("height", "50px");
+        this.closeSettingButton.style("background-color", "#f6eee3");
+        this.closeSettingButton.style("border-color", "#f6eee3");
+        this.closeSettingButton.style("color", "#47280e");
+        this.closeSettingButton.style("font-size", "50px");
+        this.closeSettingButton.hide();
+
+        this.backToMenuButton = createButton('Back to Main Menu');
+        this.backToMenuButton.mouseClicked(() => { this.backToMenuButtonClicked() });
+        this.backToMenuButton.size(100, 50);
+        this.backToMenuButton.position(W / 2 - this.backToMenuButton.size().width / 2, (H / 2) + 200);
+        this.backToMenuButton.hide();
+
+        //Setting Sliders
+        //Music Volume Control
+        this.musicSlider = createSlider(0, 1, 0.05, 0.01);
+        this.musicSlider.position(700, 460);
+        this.musicSlider.style("width", "200px");
+        this.musicSlider.style("background-color", "black");
+        this.musicSlider.hide();
+        
+        //Sound Volume Control
+        this.soundSlider = createSlider(0, 1, 0.1, 0.01);
+        this.soundSlider.position(700, 530);
+        this.soundSlider.style("width", "200px");
+        this.soundSlider.style("background-color", "black");
+        this.soundSlider.hide();
+        
+        //Setting Check Boxes
+        //Music Checker
+        this.musicCheck = createCheckbox("", false);
+        this.musicCheck.position(650, 463);
+        this.musicCheck.hide();
+        
+        //Sound Checker
+        this.soundCheck = createCheckbox("", false);
+        this.soundCheck.position(650, 533);
+        this.soundCheck.hide();
+    }
+
+    settingSprites() {
+        this.settingPopUp = new Sprite(800, 450);
+        this.settingPopUp.w = 400;
+        this.settingPopUp.h = 600;
+        this.settingPopUp.draw = () => {
+            fill(0, 0, 0, 150);
+            rect(0, -1000 + (movingAni * 2), 400, 600);
+        }
+        this.settingPopUp.collider = 'n';
+        this.settingPopUp.visible = false;
+    }
+
+    buttonSpritesFunctions() {
+        if (this.playButtonSprite.mouse.presses()) {
+            this.playButtonClicked();
+        }
+        if (this.settingButtonSprite.mouse.presses()) {
+            this.settingButtonClicked();
+        }
+        if (this.creditsButtonSprite.mouse.presses()) {
+            this.creditsButtonClicked();
+        }
+
+        //Opening and Closing Setting
+        //Animations
+        if (startMenuAni == true) {
+            movingAni -= 30;
+            if (movingAni <= 0) {
+                movingAni = 0;
+                startMenuAni = false;
+            }
+        }
+        if (openSetting == true) {
+            movingAni += 30;
+            if (movingAni >= 500) {
+                movingAni = 500;
+                openSetting = false;
+            }
+        }
+        //Buttons Visibility
+        if (activateSetting == true) {
+            this.settingPopUp.visible = true;
+        } else if (startMenuAni == false && activateSetting == false) {
+            this.settingPopUp.visible = false;
+        }
+        if (openSetting == false && activateSetting == true) {
+            this.closeSettingButton.show();
+        } else if (activateSetting == false) {
+            this.closeSettingButton.hide();
+        }
+    }
+
+    disabledMenuButtons() {
+        this.creditsButtonSprite.visible = false;
+        this.playButtonSprite.visible = false;
+        this.settingButtonSprite.visible = false;
+
+        this.creditsButtonSprite.collider = 'n';
+        this.playButtonSprite.collider = 'n';
+        this.settingButtonSprite.collider = 'n';
+    }
+
+    enableMenuButtons() {
+        this.creditsButtonSprite.visible = true;
+        this.playButtonSprite.visible = true;
+        this.settingButtonSprite.visible = true;
+
+        this.creditsButtonSprite.collider = 'k';
+        this.playButtonSprite.collider = 'k';
+        this.settingButtonSprite.collider = 'k';
+
+        // Activate The Anmiation for the Menu Buttons
+        startMenuAni = true;
+        movingAni = 500;
+    }
+
+    playButtonClicked() {
+        currentScreen = GAME;
+
+        // Old Buttons
+        // this.playButton.hide();
+        // this.settingButton.hide();
+        // this.creditsButton.hide();
+        // this.quitButton.show();
+
+        // New Sprite Buttons
+        this.disabledMenuButtons();
+    }
+
+    // Should add quit game button in the game and not in menu
+    // quitButtonClicked() { // reset game parameters here (needs to be added)
+    //     currentScreen = MENU;
+    //     playButton.show();
+    //     quitButton.hide();
+    //     creditsButton.show();
+
+    //     isGameActive = false;
+    //     allSprites.remove();
+    // }
+
+    creditsButtonClicked() {
+        currentScreen = CREDITS;
+        // Old Buttons
+        // this.playButton.hide();
+        // this.settingButton.hide();
+        // this.creditsButton.hide();
+        // this.backToMenuButton.show();
+
+        // New Sprite Buttons
+        this.backToMenuButton.show();
+        this.disabledMenuButtons();
+    }
+
+    backToMenuButtonClicked() {
+        currentScreen = MENU;
+        this.backToMenuButton.hide();
+        this.enableMenuButtons();
+    }
+
+    settingButtonClicked() {
+        //setting stuffs may be???
+        activateSetting = true;
+
+        //Activate Animation
+        movingAni = 0;
+        openSetting = true;
+    }
+
+    closeSettingClicked() {
+        // this.settingPopUp.visible = false;
+        activateSetting = false;
+
+        this.creditsButtonSprite.collider = 'k';
+        this.playButtonSprite.collider = 'k';
+        this.settingButtonSprite.collider = 'k';
+
+        startMenuAni = true;
+        movingAni = 500;
+        // this.enableMenuButtons();
     }
 }
 
@@ -58,7 +388,7 @@ class Credits {
     }
 
     preload() {
-
+        this.loadingImg = loadImage("assets/menu/loading.jpg");
     }
 
     setup() {
@@ -66,6 +396,18 @@ class Credits {
     }
 
     draw() {
-        text('credits screen', 75, 75);
+        //Background Image
+        image(this.loadingImg, 0, 0, W, H);
+
+        //Loading Text
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        fill('white');
+        text('credits screen', W / 2, 75);
+
+        //Revert Back to Normal
+        textAlign(CENTER, CENTER);
+        textSize(10);
+        fill('black');
     }
 }
